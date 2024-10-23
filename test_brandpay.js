7dfc9024dcb5bb20b36f1f5cf00da04f7fc7f6b0
Jang-Angol <75571207+Jang-Angol@users.noreply.github.com>
2024-10-23 09:24:51 +0900

    Create test_brandpay.js

---
+++ test_brandpay.js
@@ -0,0 +1,51 @@
+// OrderPaymentWidget 클래스 정의
+class OrderPaymentWidget {
+    // 결제위젯 객체 초기화
+    constructor(clientKey, customerKey) {
+        // sdk 초기화
+        const tosspayments = TossPayments(clientKey);
+
+        customerKey = (customerKey === "") ? TossPayments.ANONYMOUS : customerKey ;
+        this.widget = tosspayments.widgets({ customerKey,
+            brandpay: {
+              redirectUrl: window.location.origin + "/Pay/Recv/landingbrand/RequestAccessToken.php?partnerid=test" + 
+          } });
+    }
+
+    // 결제금액 설정
+    async setAmount(currency, amount) {
+        await this.widget.setAmount({ value: amount, currency: currency });
+    }
+
+    // 결제위젯 UI 렌더링
+    async renderPaymentWidget(divId, variantKey) {
+        try {
+            return await this.widget.renderPaymentMethods({ selector: divId, variantKey: variantKey });
+        } catch (error) {
+            console.error('Failed to render payment widget:', error);
+        }
+    }
+
+    // 결제요청
+    requestPayment(orderId, orderName, successUrl, failUrl, customerEmail, customerName, customerMobilePhone, taxFreeAmount) {
+        try {
+            console.log(orderId, orderName, successUrl, failUrl, customerEmail, customerName, customerMobilePhone, taxFreeAmount);
+            this.widget.requestPayment({
+                orderId: orderId,
+                orderName: orderName,
+                successUrl: successUrl,
+                failUrl: failUrl,
+                customerEmail: customerEmail,
+                customerName: customerName,
+                customerMobilePhone: customerMobilePhone,
+                taxFreeAmount: taxFreeAmount
+            });
+        } catch (error) {
+            console.error('Payment request failed:', error);
+        }
+    }
+}
+
+// 전역에 OrderPaymentWidget 할당
+window.OrderPaymentWidget = OrderPaymentWidget;
+console.log('brandpay');
